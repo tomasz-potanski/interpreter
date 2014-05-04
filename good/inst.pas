8e95c6bf92d4388e855402206ca2df2b98f1253a
Tomasz Potanski <tomasz@potanski.pl>
2014-05-04 13:38:17 +0200

    (nie, 4 maj 2014, 13:38:17 CEST)

---
+++ good/inst.pas
@@ -0,0 +1,21 @@
+begin
+
+	dwanascie := 3 ;
+	dwanascie *= 4 ;
+
+	dwa := 8 ;
+	dwa /= 4;
+
+	jeden := 3;
+	jeden -=2;
+
+	trzy := 1;
+	trzy +=2;
+
+	osiem:=7;
+	++osiem;
+
+	szesc:=7;
+	--szesc;
+
+end
---
+++ good/stmts.pas
@@ -0,0 +1,26 @@
+begin
+	x := 3 + 2 ;
+	x := x + 1 ;
+	y := x + 3 ;
+	z := 1 ;
+	if z < 2 then print "OK" ;
+	if z < 3 then print z ;
+	print 'a';
+	print 3.1415;
+	
+	n := 10 ;
+	counter := 0 ;
+	i := 0 ;
+	while i < n do
+	begin
+		i := i + 1 ;
+		counter := counter + 1;
+		counter := counter + 1;
+	end
+
+	dwa := 8 / 4;
+	trzy := 4 - 1;
+	cztery := 1 + 3;
+	dziesiec := 2 * 5;
+
+end.
