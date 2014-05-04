5de09f4e913ad5294da0887e716ca673a9538996
Tomasz Potanski <tomasz@potanski.pl>
2014-05-04 08:33:57 +0200

    (nie, 4 maj 2014, 08:33:57 CEST)

--- Interpreterr.hs
+++ Interpreterr.hs
@@ -1,7 +1,5 @@
 module Interpreterr where
 import Absactualgrammar
-import Control.Monad.State
-import Control.Monad.Error
 import qualified Data.Map as M
 --------------------------------
 --------------------------------
@@ -13,10 +11,13 @@ import qualified Data.Map as M
 --------------------------------
 --------------------------------
 
-type State = Map.Map String Integer
+type TState = Map.Map String Integer
 -- nazwa zmiennej -> wartosc
 
-interpretExp :: Exp -> State -> Integer
+--import Control.Monad.TState
+--import Control.Monad.Error
+
+interpretExp :: Exp -> TState -> Integer
 interpretExp x s = case x of
   EAdd exp0 exp  -> (interpretExp exp0 s) + (interpretExp exp s)
   ESub exp0 exp  -> (interpretExp exp0 s) - (interpretExp exp s)
@@ -27,7 +28,7 @@ interpretExp x s = case x of
 	Just n 	-> n
 	Nothing	-> 0 -- !!rzuc blad
 
-interpretBExp :: BExp -> State -> Bool
+interpretBExp :: BExp -> TState -> Bool
 interpretBExp b s = case b of
 	BOr bexp1 bexp2 -> (interpretBExp bexp1 s) || (interpretBExp bexp2 s) 
 	BAnd bexp1 bexp2 -> (interpretBExp bexp1 s) && (interpretBExp bexp2 s) 
@@ -35,14 +36,14 @@ interpretBExp b s = case b of
 		LTH -> (interpretExp exp1 s) < (interpretExp exp2 s)
 		GTH -> (interpretExp exp1 s) > (interpretExp exp2 s)
 
-interpretStmt :: Stmt -> State -> State
+interpretStmt :: Stmt -> TState -> TState
 interpretStmt stmt s = case stmt of
     IAss (Ident x) exp ->
         let val = (interpretExp exp s)
         in M.insert x val s
     IIf b i ->
         let cond = (interpretBexp b s)
-        in if cond then (interpretStmt i s else s)
+        in if cond then (interpretStmt i s) else s
     IWhl b i ->
         let cond = (interpretBexp b s)
         in if cond then (interpretStmt instr (interpretStmt i s)) else s
@@ -50,6 +51,6 @@ interpretStmt stmt s = case stmt of
     IBlk (i:is) ->
         (interpretStmt (IBlk is) (interpretStmt i s))
 
-interpretFile :: Stmt -> State
+interpretFile :: Stmt -> TState
 interpretFile i = interpretStmt i M.empty
---interpretFile :: Program -> State
+--interpretFile :: Program -> TState
