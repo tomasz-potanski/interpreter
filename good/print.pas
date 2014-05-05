e23ab330fd0fa117de2d5d44a3eace673df8554b
Tomasz Potanski <tomasz@potanski.pl>
2014-05-05 21:44:42 +0200

    (pon, 5 maj 2014, 21:44:42 CEST)

--- good/print.pas
+++ good/print.pas
@@ -6,19 +6,29 @@ var
 	x : Integer ;
 	y : String;
 	z : Boolean;
+	a : Array[1..10] of Integer;
+	b : Array[1..10] of String;
+	c : Array[1..10] of Boolean;
 
 begin
-	print <- ("Ala ma kota!");
-	print <- (True);
-	print <- (False);
+//	print <- ("Ala ma kota!");
+//	print <- (True);
+//	print <- (False);
 	x := 0;
-	y := "... ale Ola jest ładniejsza ;)";
+	y := "... ale Ola jest ladniejsza ;)";
 	z := True;
 	print <-- (x);
 	print <-- (y);
 	print <-- (z);
 
-/*	x := 1;
+	a[1] := 0;
+	b[1] := "... ale Ola jest ladniejsza ;)";
+	c[1] := True;
+/*	print <-- (a[1]);
+	print <-- (b[1]);
+	print <-- (c[1]);
+
+	x := 1;
 	print <-- (x);
 
 	x := 2;
