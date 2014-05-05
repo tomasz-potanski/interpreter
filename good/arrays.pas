dc139b96cf1ecbd47e3f78fa7f70cc3b8a8a7b0d
Tomasz Potanski <tomasz@potanski.pl>
2014-05-05 08:51:18 +0200

    (pon, 5 maj 2014, 08:51:18 CEST)

--- good/arrays.pas
+++ good/arrays.pas
@@ -21,14 +21,17 @@ begin
 	table[6]:=6;
 	table[7]:=7;
 	table[8]:=8;
-//	print table[5];
-//	print table[6];
-//	print table[7];
+	print table[5];
+	print table[6];
+	print table[7];
 
 	tableBool[1] := 2 > 1;
 	tableBool[2] := 4 == 4;
 	b1:=True;
+
+	if True then print "OK";
+
 	print b1;
-	//print tableBool[1];
-	//print tableBool[2];
+	print tableBool[1];
+	print tableBool[2];
 end.
