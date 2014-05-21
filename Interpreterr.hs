20238499c351cffa4f18e6ca82b21c109a6e266c
Tomasz Potanski <tomasz@potanski.pl>
2014-05-22 01:53:26 +0200

    (czw, 22 maj 2014, 01:53:26 CEST)

--- Interpreterr.hs
+++ Interpreterr.hs
@@ -442,7 +442,8 @@ interpretBExp b s@(state, funcMap) = case b of
                 TTArray _ _ _ _ -> error("Error - type mismatch")
                 TTString str2 -> case (checkifVarExists (Ident y) s) of
                         False -> error("Error - Variable: " ++ (show x) ++ " has not been declared!")
-                        True -> case (M.lookup y state) of Nothing -> error("Error - Variable: " ++ (show y) ++ " has not been declared!")
+                        True -> case (M.lookup y state) of
+                                    Nothing -> error("Error - Variable: " ++ (show y) ++ " has not been declared!")
                                     Just nn -> case nn of TTBoolean _ -> error("Error - type mismatch")
                                         TTInt _ -> error("Error - type mismatch")
                                         TTVoid -> error("Error - type mismatch")
