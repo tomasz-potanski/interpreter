a9642cf5f07e33d4e098e0df2e695c407fa98659
Tomasz Potanski <tomasz@potanski.pl>
2014-05-22 04:35:03 +0200

    (czw, 22 maj 2014, 04:35:03 CEST)

--- good/funs2.pas
+++ good/funs2.pas
@@ -2,10 +2,10 @@ var
 	b : Boolean;
 	s : String;
 	I : Integer;
-	tabI : Array[0..10] of Integer;
+	tabI : Array[0..10] of String;
 proc
 
-	function druk(zm : Integer;) : Array[0..10] of String;
+	function druk(zm : String;) : Array[0..10] of String;
 	begin
 		print <-- (zm);
 		druk[0] := "Dzialam";
@@ -15,6 +15,6 @@ begin
 	b := True;
 	s := "ala ma kota...";
 	I := 34;
-	tabI[1] := 3;
+	tabI[1] := "sdfsd";
 	print <-- ( druk(tabI[1]) );
 end.
