3c49eebb0734d5f882eaf95a3cf699d94b739eb1
Tomasz Potanski <tomasz@potanski.pl>
2014-05-22 02:06:17 +0200

    (czw, 22 maj 2014, 02:06:17 CEST)

--- Interpreterr.hs
+++ Interpreterr.hs
@@ -443,15 +443,15 @@ interpretBExp b s@(state, funcMap) = case b of
 			TTVoid -> error("Error - type mismatch")
 			TTArray _ _ _ _ -> error("Error - type mismatch")
 			TTString str2 -> case (checkifVarExists (Ident x) s) of
-                False -> error("Error - Variable: " ++ (show x) ++ " has not been declared!")
-                True -> case (M.lookup x state) of
-                    Nothing -> error("Error - Variable: " ++ (show x) ++ " has not been declared!")
-                    Just n -> case n of
-                    TTBoolean _ -> error("Error - type mismatch")
-                    TTInt _ -> error("Error - type mismatch")
-                    TTVoid -> error("Error - type mismatch")
-                    TTArray _ _ _ _ -> error("Error - type mismatch")
-                    TTString str ->	if str == str2 then True else False
+            False -> error("Error - Variable: " ++ (show x) ++ " has not been declared!")
+            True -> case (M.lookup x state) of
+                Nothing -> error("Error - Variable: " ++ (show x) ++ " has not been declared!")
+                Just n -> case n of
+                TTBoolean _ -> error("Error - type mismatch")
+                TTInt _ -> error("Error - type mismatch")
+                TTVoid -> error("Error - type mismatch")
+                TTArray _ _ _ _ -> error("Error - type mismatch")
+                TTString str ->	if str == str2 then True else False
 
 
 	BExpArray (Ident x) index -> case (checkifVarExistsAndIsArray (Ident x) s) of
