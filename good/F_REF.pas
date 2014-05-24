43544f1a05f181b1607c82aa4fab60ee5c31375a
Tomasz Potanski <tomasz@potanski.pl>
2014-05-24 13:56:51 +0200

    (sob, 24 maj 2014, 13:56:51 CEST)

--- good/F_REF.pas
+++ good/F_REF.pas
@@ -1,5 +1,8 @@
 var
 	x : Integer;
+	s : String;
+	b : Boolean;
+	tab : Array[0..10] of Integer;
 
 proc
 	procedure zwieksz( a : Integer ;);
@@ -14,6 +17,42 @@ proc
 		print <-- (a);
 	end
 
+	procedure zwiekszA( a : Array [1..10] of Integer ;);
+	begin
+		a[1] := a + 7;
+		print <-- (a[1]);
+	end
+
+	procedure zwiekszA2( a : Array [1..10] of Integer ;);
+	begin
+		a[1] := a + 7;
+		print <-- (a[1]);
+	end
+
+	procedure changeS( a : String ;);
+	begin
+		a := "Po przypisaniu w funkcji";
+		print <-- (a);
+	end
+
+	procedure changeS2( a : String ;);
+	begin
+		a := "Po przypisaniu w funkcji";
+		print <-- (a);
+	end
+
+	procedure changeB( a : Boolean ;);
+	begin
+		a := True;
+		print <-- (a);
+	end
+
+	procedure changeB2( a : Boolean ;);
+	begin
+		a := True;
+		print <-- (a);
+	end
+
 begin
 	x := 10;
 	zwieksz(x);
@@ -21,5 +60,26 @@ begin
 
 	zwieksz2(ref x);
 	print <-- (x);
+
+	s := "Przed wywolaniem";
+	changeS(s);
+	print <-- (s);
+
+	changeS2(ref s);
+	print <-- (s);
+
+	b := False;
+	changeB(b);
+	print <-- (b);
+
+	changeB2(ref b);
+	print <-- (b);
+
+	tab[1] := 10;
+	zwiekszA(tab);
+	print <-- (tab[1]);
+
+	zwiekszA(ref tab);
+	print <-- (tab[1]);
 	
 end.
