0edd180dbf37735d37046c6e7ee1477e0921a0f6
Tomasz Potanski <tomasz@potanski.pl>
2014-05-24 14:36:09 +0200

    (sob, 24 maj 2014, 14:36:09 CEST)

--- Interpreterr.hs
+++ Interpreterr.hs
@@ -1506,7 +1506,7 @@ simpleAddOneVar (Ident x) value (state, funcMap) = ((M.insert x value state), fu
 
 simpleAddOneRec :: Ident -> [VarDeclarationLine] -> TState3 -> TState3
 simpleAddOneRec (Ident x) [] s@(state, funcMap) = s
-simpleAddOneRec (Ident x) list@((DLList ((Ident xx):[]) typee):T) s@(state, funcMap) = case (M.lookup x state) of
+simpleAddOneRec (Ident x) list@((DLList ((Ident xx):[]) typee):_) s@(state, funcMap) = case (M.lookup x state) of
     Nothing -> ((M.insert x (TTRecord (M.insert xx (typeToDefaultTType typee) M.empty)) state) , funcMap)
     Just value -> error("Error - not implemented")
 
