11d7f39f2ab8a774288c5b316343af1602f78d52
Tomasz Potanski <tomasz@potanski.pl>
2014-05-24 12:19:02 +0200

    (sob, 24 maj 2014, 12:19:02 CEST)

--- Interpreterr.hs
+++ Interpreterr.hs
@@ -1355,20 +1355,21 @@ interpretStmt stmt s@(extState, funcMap) = case stmt of
 	            EmptyArgs -> error ("Error - arguments were given!")
 
 
-    SProcCallString (Ident x) strstr -> case (M.lookup x funcMap) of
-        Nothing -> error("Error - Functin/procedure: "++ (show x)++" has not been found!")
-        Just (stmt, varDeclarationLine, tTypes, tStateOld) ->
-            let globals = M.intersection extState tStateOld
-            in
-	        case varDeclarationLine of {
-	            NonEmptyArgs v -> case v of {;
-	                DLList identList@((Ident ident):_) typee -> case typee of {;;
-	                    TString -> ( M.union (M.intersection (fst (interpretStmt stmt (M.insert ident (TTString strstr) (M.union tStateOld extState) , funcMap))) globals) extState, funcMap);;;
-	                    otherwise -> error("Error - incorrect type") ;;;
-	                };;
-	            };
-	            EmptyArgs -> error ("Error - arguments were given!") ;
-	        }
+    SProcCallString (Ident x) strstr -> case (M.lookup x funcMap) of {
+        Nothing -> error("Error - Functin/procedure: "++ (show x)++" has not been found!");
+        Just (stmt, varDeclarationLine, tTypes, tStateOld) ->;
+            let globals = M.intersection extState tStateOld;
+            in;
+	        case varDeclarationLine of {;
+	            NonEmptyArgs v -> case v of {;;
+	                DLList identList@((Ident ident):_) typee -> case typee of {;;;
+	                    TString -> ( M.union (M.intersection (fst (interpretStmt stmt (M.insert ident (TTString strstr) (M.union tStateOld extState) , funcMap))) globals) extState, funcMap);;;;
+	                    otherwise -> error("Error - incorrect type") ;;;;
+	                };;;
+	            };;
+	            EmptyArgs -> error ("Error - arguments were given!") ;;
+	        };
+	 }
 
 
 ----TODO
