cd170e9a2966d832678c1c0285464ca9725a0946
Tomasz Potanski <tomasz@potanski.pl>
2014-05-24 11:50:54 +0200

    (sob, 24 maj 2014, 11:50:54 CEST)

--- Interpreterr.hs
+++ Interpreterr.hs
@@ -1396,19 +1396,19 @@ interpretStmt stmt s@(extState, funcMap) = case stmt of
 ----TODO
 	SProcCallFuncSyg (Ident x) funcDeclLine -> case (M.lookup x funcMap) of
 	    Nothing -> error("Error - function " ++ (show x) ++ "could not be found!")
-	    Just (stmt, varDeclarationLine, tTypes, tStateOld) ->
-	        let globals = M.intersection extState tStateOld
-	        in
-	        case varDeclarationLine of
-	            EmptyArgs -> error("Error - function " ++ (show x) ++ " is supposed to be given arguments.")
-	            NonEmptyArgs identList@((Ident identArg):_) typee ->
-	                if not(isAFunctionType(typee)) then
-	                    error("Error - function " ++ (show x) ++ " is suppose to take a function.")
-	                else
-	                    if (funcDeclTypeOK funcDeclLine typee) then
-	                        error("Error --- type mismatch!")
-	                    else
-	                        ( M.union (M.intersection (fst (interpretStmt stmt ((M.union tStateOld extState) , (addOneFunction funcDeclLine funcMap)))) globals) extState, funcMap)
+	    Just (stmt, varDeclarationLine, tTypes, tStateOld) -> error("Not implemented yet!")
+--	        let globals = M.intersection extState tStateOld
+--	        in
+--	        case varDeclarationLine of
+--	            EmptyArgs -> error("Error - function " ++ (show x) ++ " is supposed to be given arguments.")
+--	            NonEmptyArgs identList@((Ident identArg):_) typee ->
+--	                if not(isAFunctionType(typee)) then
+--	                    error("Error - function " ++ (show x) ++ " is suppose to take a function.")
+--	                else
+--	                    if (funcDeclTypeOK funcDeclLine typee) then
+--	                        error("Error --- type mismatch!")
+--	                    else
+--	                        ( M.union (M.intersection (fst (interpretStmt stmt ((M.union tStateOld extState) , (addOneFunction funcDeclLine funcMap)))) globals) extState, funcMap)
 
 
 
