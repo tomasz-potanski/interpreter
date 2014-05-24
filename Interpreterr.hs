59c27e593995350c665a013027a54dd39ff5adf2
Tomasz Potanski <tomasz@potanski.pl>
2014-05-24 18:29:54 +0200

    (sob, 24 maj 2014, 18:29:54 CEST)

--- Interpreterr.hs
+++ Interpreterr.hs
@@ -1642,7 +1642,7 @@ addOneProc h funcMap = case h of
 
 
 addOneProc2 :: ProcDeclLine -> TState3 -> TState3
-addOneProc h state@(s, funcMap) = case h of
+addOneProc2 h state@(s, funcMap) = case h of
     PLineNonArg (Ident x) varDecls stmt  -> (s, M.insert x (stmt, EmptyArgs, TTVoid, (fst (declareNewVariables varDecls (M.empty, M.empty)))) funcMap)
     PLineArg	(Ident x) args varDecls stmt  -> (s, M.insert x (stmt, (NonEmptyArgs args), TTVoid, (fst (declareNewVariables varDecls (M.empty, M.empty)))) funcMap)
 
