0115948dc21d698f888857d1ca18d2f8add770ff
Tomasz Potanski <tomasz@potanski.pl>
2014-05-24 19:22:00 +0200

    (sob, 24 maj 2014, 19:22:00 CEST)

--- Interpreterr.hs
+++ Interpreterr.hs
@@ -1560,7 +1560,7 @@ interpretStmt stmt s@(extState, funcMap) = case stmt of
 	                            error("Error - incorrect types!")
 	            EmptyArgs -> error ("Error - arguments were given!")
 
-    SFuncDeclLine funcDeclLine -> addOneFunction2 s
+    SFuncDeclLine funcDeclLine -> addOneFunction2 funcDeclLine s
 
 -----------------------------------------
 --type TFuncDef = (Stmt, VarDeclarationLine, TTypes, TStateOld)
